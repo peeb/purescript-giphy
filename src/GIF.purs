d785fc8fa2d12e313e281cac6e7a13f5442560be
Paul Burt <paul.burt@gmail.com>
2018-06-01 15:07:49 +0100

    Added Halogen.Themes.Bulma to abstract Bulma CSS

--- src/GIF.purs
+++ src/GIF.purs
@@ -10,6 +10,7 @@ import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events  as HE
 import Halogen.HTML.Properties as HP
+import Halogen.Themes.Bulma as HB
 
 import Network.HTTP.Affjax (AJAX)
 
@@ -43,35 +44,43 @@ ui =
 
   render :: State -> H.ComponentHTML Query
   render { loading, result, searchTerm } =
-    HH.section [ HP.class_ $ H.ClassName "container" ] $
-      [ HH.div [ HP.class_ $ H.ClassName "field" ]
-          [ HH.div [ HP.class_ $ H.ClassName "control" ]
+    HH.section
+      [ HP.class_ HB.container ] $
+      [ HH.div
+          [ HP.class_ HB.field ]
+          [ HH.div
+              [ HP.class_ HB.control ]
               [ HH.input
-                  [ HP.class_ $ H.ClassName "input"
+              [ HP.class_ HB.input
                   , HP.placeholder "Enter search term"
                   , HE.onValueInput $ HE.input SetSearchTerm
                   , HP.value searchTerm
                   ]
               ]
           ]
-       , HH.div [ HP.class_ $ H.ClassName "control" ]
-           [ HH.button
-               [ HP.class_ $ H.ClassName "button"
-               , HP.disabled loading
-               , HE.onClick $ HE.input_ MakeRequest
-               ]
-               [ HH.text "Go!" ]
-           ]
-       , HH.section [ HP.class_ $ H.ClassName "container" ]
-           case result of
-             Nothing -> []
-             Just (GIF { title, url }) ->
-               [ HH.img
-                   [ HP.alt title
-                   , HP.src url
-                   , HP.title title
-                   ]
-               ]
+      , HH.div
+          [ HP.class_ HB.control ]
+          [ HH.button
+              [ HP.classes [ HB.button, HB.isPrimary ]
+              , HP.disabled loading
+              , HE.onClick $ HE.input_ MakeRequest
+              ]
+              [ HH.text "Go!" ]
+          ]
+      , HH.div
+          [ HP.class_ HB.container ]
+          case result of
+            Nothing -> []
+            Just (GIF { title, url }) ->
+              [ HH.figure
+                  [ HP.classes [ HB.figure, HB.is3By2 ] ]
+                  [ HH.img
+                      [ HP.alt title
+                      , HP.src url
+                      , HP.title title
+                      ]
+                  ]
+              ]
       ]
 
   eval :: Query ~> H.ComponentDSL State Query Void (Aff (ajax :: AJAX | eff))
---
+++ src/Halogen/Themes/Bulma.purs
@@ -0,0 +1,27 @@
+module Halogen.Themes.Bulma where
+
+import Halogen.HTML (ClassName(..))
+
+button :: ClassName
+button = ClassName "button"
+
+container :: ClassName
+container = ClassName "container"
+
+control :: ClassName
+control = ClassName "control"
+
+field :: ClassName
+field = ClassName "field"
+
+figure :: ClassName
+figure = ClassName "figure"
+
+input :: ClassName
+input = ClassName "input"
+
+is3By2 :: ClassName
+is3By2 = ClassName "is-3by2"
+
+isPrimary :: ClassName
+isPrimary = ClassName "is-primary"
